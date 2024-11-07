e4da610547bbaa9be139a8dac2177229cde3defa
Tarindeka <tndkpaul@gmail.com>
2024-11-07 21:15:36 +0300

    Add files via upload

---
+++ classroom_management/credentials_and_instructions.php
@@ -0,0 +1,12 @@
+\*To run my classroom management system project, you have to first import the cms_db.sql file to your myphp admin database.
+Once you are on the login page of the system, the credentials are;
+
+If you want to login as an ADMINISTRATOR: 
+Username: admin
+Password: admin123
+
+If you want to login as a STUDENT:
+Username: student1
+Password: student123
+
+*\             
